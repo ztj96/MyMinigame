904fe8edb267c5857fa82e86ac0e7fafde6e3384
ztjztj96 <ztjztj96@gmail.com>
2015-07-31 20:05:31 +0800

    Init
    Init unity 4.6.1

---
+++ Assets/people.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetButtonDown("Jump")) {
+		transform.Translate(0, -2, 0);
+	}
+	if (Input.GetButtonUp("Jump")) {
+		transform.Translate(0,2,0);
+	}
+}
